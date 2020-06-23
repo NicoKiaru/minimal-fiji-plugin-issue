4f32382d2191e976b23229fa84731a2e0190af71
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-06-23 12:44:47 +0200

    use of AbstractDelegateConverter thanks to @imagejan

--- src/main/java/org/debugdemo/debug/ui/StringToDatasetConverter.java
+++ src/main/java/org/debugdemo/debug/ui/StringToDatasetConverter.java
@@ -3,27 +3,14 @@ package org.debugdemo.debug.ui;
 import ij.ImagePlus;
 import net.imagej.Dataset;
 import org.scijava.Priority;
-import org.scijava.convert.AbstractConverter;
-import org.scijava.convert.ConvertService;
-import org.scijava.plugin.Parameter;
+import org.scijava.convert.AbstractDelegateConverter;
+import org.scijava.convert.Converter;
 import org.scijava.plugin.Plugin;
 
-@Plugin(type = org.scijava.convert.Converter.class, priority = Priority.EXTREMELY_HIGH) // priority in order to bypass scifio converter to come...
-public class StringToDatasetConverter extends AbstractConverter<String, Dataset> {
-
-    @Parameter
-    ConvertService cs;
-
-    @Override
-    public <T> T convert(Object src, Class<T> dest) {
-        assert src instanceof String;
-        String name = (String) src;
-        // First conversion : String to ImagePlus
-        ImagePlus imagePlus = cs.convert(name, ImagePlus.class);
-        if (imagePlus == null) return null;
-        // Second convestion : ImagePlus to Dataset
-        return (T) cs.convert(imagePlus, Dataset.class);
-    }
+@Plugin(type = Converter.class, priority = Priority.VERY_HIGH) // don't exaggerate priorities!
+public class StringToDatasetConverter extends
+        AbstractDelegateConverter<String, ImagePlus, Dataset>
+{
 
     @Override
     public Class<Dataset> getOutputType() {
@@ -33,5 +20,10 @@ public class StringToDatasetConverter extends AbstractConverter<String, Dataset>
     @Override
     public Class<String> getInputType() {
         return String.class;
+    }
+
+    @Override
+    protected Class<ImagePlus> getDelegateType() {
+        return ImagePlus.class;
     }
 }
