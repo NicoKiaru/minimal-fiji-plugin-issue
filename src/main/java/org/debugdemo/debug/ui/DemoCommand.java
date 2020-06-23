e4fe4f54a9cb4e89f2c5f19d04b8cc612f5c596e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-06-23 12:05:43 +0200

    adding converter from String to Dataset as a workaround

--- src/main/java/org/debugdemo/debug/ui/DemoCommand.java
+++ src/main/java/org/debugdemo/debug/ui/DemoCommand.java
@@ -1,13 +1,11 @@
 package org.debugdemo.debug.ui;
 
+import ij.IJ;
 import net.imagej.Dataset;
-import org.scijava.app.StatusService;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
-import org.scijava.options.OptionsService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.ui.UIService;
 import net.imagej.ImageJ;
 
 @Plugin(type = Command.class, headless = true, menuPath = "Plugins>debug-demo>run-demo")
@@ -15,15 +13,6 @@ public class DemoCommand implements Command {
 
     @Parameter
     public LogService logService;
-
-    @Parameter
-    public StatusService statusService;
-
-    @Parameter
-    public OptionsService optionsService;
-
-    @Parameter
-    public UIService uiService;
 
     @Parameter(label = "Image 1")
     public Dataset inputImage1;
@@ -33,11 +22,21 @@ public class DemoCommand implements Command {
 
     @Override
     public void run() {
-        logService.info("Ran - nothing should happen");
+        logService.info("First image : "+inputImage1.getName());
+        logService.info("Second image : "+inputImage2.getName());
     }
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
+
+        IJ.openImage("src/main/resources/one.png").show();
+        IJ.openImage("src/main/resources/two.png").show();
+
+        String scriptTest = "run(\"run-demo\", \"inputimage1=one.png inputimage2=two.png\");";
+
+        ij.script().run("test.ijm",scriptTest,true).get();
+
     }
 }
